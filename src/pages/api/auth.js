5548cb08e5cddf4404fa8399bcd1370956566bf2
oleh-domshynskyi <64157303+oleh-domshynskyi@users.noreply.github.com>
2021-11-12 19:53:19 +0200

    feat: fix config

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -1,27 +1,53 @@
 import { AuthorizationCode } from 'simple-oauth2';
-import { randomBytes } from 'crypto';
 import { config } from '../../../lib/config';
-import { scopes } from '../../../lib/scopes';
-
-export const randomString = () => randomBytes(4).toString('hex');
 
 export default async (req, res) => {
   const { host } = req.headers;
   const url = new URL(`https://${host}/${req.url}`);
   const urlParams = url.searchParams;
+  const code = urlParams.get('code');
   const provider = urlParams.get('provider');
-
-  // simple-oauth will use our config files to generate a client we can use to request access
+  // we recreate the client we used to make the request
   const client = new AuthorizationCode(config(provider));
-  // we then make a build the request to our provider
-  const authorizationUri = client.authorizeURL({
-    // your callback url is important! More on this later
+  // create our token object
+  const tokenParams = {
+    code,
     redirect_uri: `https://${host}/api/callback?provider=${provider}`,
-    scope: scopes[provider],
-    state: randomString(),
-  });
+  };
+  try {
+    // try to create an access token from the client
+    const accessToken = await client.getToken(tokenParams);
+    const token = accessToken.token['access_token'];
 
-  // and get redirected to Github for authorisation
-  res.writeHead(301, { Location: authorizationUri });
-  res.end();
+    const responseBody = renderBody('success', {
+      token,
+      provider,
+    });
+
+    res.statusCode = 200;
+    res.end(responseBody);
+  } catch (e) {
+    res.statusCode = 200;
+    res.end(renderBody('error', e));
+  }
 };
+
+// This renders a simple page with javascript that allows the pop-up page
+// to communicate with its opener
+function renderBody(status, content) {
+  return `
+    <script>
+      const receiveMessage = (message) => {
+        window.opener.postMessage(
+          'authorization:${content.provider}:${status}:${JSON.stringify(
+    content,
+  )}',
+          message.origin
+        );
+        window.removeEventListener("message", receiveMessage, false);
+      }
+      window.addEventListener("message", receiveMessage, false);
+      window.opener.postMessage("authorizing:${content.provider}", "*");
+    </script>
+  `;
+}
